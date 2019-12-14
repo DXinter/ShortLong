122269ef8615ec8ca5952135044639788d6c1eca
DXinter <58430974+DXinter@users.noreply.github.com>
2019-12-14 23:32:20 +0300

    Add files via upload

---
+++ Mixed.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShortLongShort
+{
+    class Mixed
+    {
+        public string ShortlongShort(string first, string second)
+        {
+            string result = "";
+            if (first.Length == 1 && second.Length > 1)
+            {
+                result += first;
+                result += second;
+                result += first;
+                return result;
+            }
+            if (first.Length > 1 && second.Length == 1)
+            {
+                result += second;
+                result += first;
+                result += second;
+                return result;
+            }
+            else
+                throw new Exception("Ошибка!\n Операция невозможна из-за отсуствия условий!");
+
+                                                                  
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ShortLongShort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mixed mixed = new Mixed();
+            string first, second;
+            try
+            {
+                Set(out first, out second);
+                ResultMethod(mixed, first, second);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+        private static void ResultMethod(Mixed mixed, string first, string second)
+        {
+            string result = mixed.ShortlongShort(first, second);
+            Console.WriteLine("Результат: {0}",result);
+        }
+
+        private static void Set(out string first, out string second)
+        {
+            Console.WriteLine("Введите первую строку:");
+            first = Console.ReadLine();
+            Console.WriteLine("Введите вторую строку");
+            second = Console.ReadLine();
+        }
+    }
+}
